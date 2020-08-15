6bea6956076ca2bbbb5bba34a2c540144d956c52
aruntony005 <64554015+aruntony005@users.noreply.github.com>
2020-08-15 00:53:55 -0700

    Create startup_master.sh

---
+++ startup_master.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+sudo mkdir /root/.ssh
+sudo echo $1 > /root/.ssh/id_rsa
+sudo echo $2 > /root/.ssh/authorized_keys
+i=`cat /etc/ssh/sshd_config | grep -n PermitRootLogin\ no | awk -F: '{print $1}'`
+sudo sed -i "$i"d /etc/ssh/sshd_config
+i=`cat /etc/ssh/sshd_config | grep -n \#PubkeyAuthentication\ yes | awk -F: '{print $1}'`
+sudo sed -i "$i"d /etc/ssh/sshd_config
+i=`cat /etc/ssh/sshd_config | grep -n PasswordAuthentication\ no | awk -F: '{print $1}'`
+sudo sed -i "$i"d /etc/ssh/sshd_config
+
+sudo echo -e "PermitRootLogin yes\nPubkeyAuthentication yes\nPasswordAuthentication yes" >> /etc/ssh/sshd_config
+
+sudo systemctl restart sshd
