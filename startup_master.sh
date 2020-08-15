f081e4ba8d5994e7e3c99c2a261b5acd4b5fc65c
aruntony005 <aruntony005@gmail.com>
2020-08-15 18:18:21 +0000

    sample

--- startup_master.sh
+++ startup_master.sh
@@ -54,5 +54,5 @@ sudo cat /etc/kubernetes/admin.conf > /root/.kube/config
 
 sudo cat /root/kubeinit | grep -E 'kubeadm join|--discovery' >> /root/kubeinit.sh
 sudo chmod 755 /root/kubeinit.sh
-sudo ssh worker < /root/kubeinit.sh
+sudo ssh -o StrictHostKeyChecking=no worker < /root/kubeinit.sh
 sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
